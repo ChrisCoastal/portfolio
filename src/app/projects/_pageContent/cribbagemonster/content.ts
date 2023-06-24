0474ecc5950a445548b5d366ae2339da4f19ece9
ChrisCoastal <contactchriscoastal@gmail.com>
2023-06-24 10:51:42 -0700

    update image paths for build

--- src/app/projects/_pageContent/cribbagemonster/content.ts
+++ src/app/projects/_pageContent/cribbagemonster/content.ts
@@ -4,11 +4,11 @@ import { code } from './codeSnippets';
 import { links } from './links';
 import { text } from './text';
 
-import cribbagemonster_image from '/public/assets/projects/cribbagemonster/images/cribbagemonster.jpg';
-import cribbagemonster_image_1 from '/public/assets/projects/cribbagemonster/images/cribbagemonster_1.jpg';
-import cribbagemonster_image_2 from '/public/assets/projects/cribbagemonster/images/cribbagemonster_2.jpg';
-import cribbagemonster_image_3 from '/public/assets/projects/cribbagemonster/images/cribbagemonster_3.jpg';
-import cribbagemonster_image_4 from '/public/assets/projects/cribbagemonster/images/cribbagemonster_4.jpg';
+// import cribbagemonster_image from '/public/assets/projects/cribbagemonster/images/cribbagemonster.jpg';
+// import cribbagemonster_image_1 from '/public/assets/projects/cribbagemonster/images/cribbagemonster_1.jpg';
+// import cribbagemonster_image_2 from '/public/assets/projects/cribbagemonster/images/cribbagemonster_2.jpg';
+// import cribbagemonster_image_3 from '/public/assets/projects/cribbagemonster/images/cribbagemonster_3.jpg';
+// import cribbagemonster_image_4 from '/public/assets/projects/cribbagemonster/images/cribbagemonster_4.jpg';
 
 const cribbagemonster: ProjectContent = {
   title: 'Cribbage Monster',
@@ -16,18 +16,27 @@ const cribbagemonster: ProjectContent = {
   projectType: 'web',
   year: 2023,
   thumbnail: {
-    image: cribbagemonster_image,
+    image: '/assets/projects/cribbagemonster/images/cribbagemonster.jpg',
     alt: 'Cribbage Monster - Are you a Monster?',
   },
   text,
   headerImages: [
-    { image: cribbagemonster_image_1, alt: 'a cribbage board' },
-    { image: cribbagemonster_image_2, alt: 'a cribbage board' },
-    { image: cribbagemonster_image_3, alt: 'a cribbage board' },
+    {
+      image: '/assets/projects/cribbagemonster/images/cribbagemonster_1.jpg',
+      alt: 'a cribbage board',
+    },
+    {
+      image: '/assets/projects/cribbagemonster/images/cribbagemonster_2.jpg',
+      alt: 'a cribbage board',
+    },
+    {
+      image: '/assets/projects/cribbagemonster/images/cribbagemonster_3.jpg',
+      alt: 'a cribbage board',
+    },
   ],
   galleryImages: [
     {
-      image: cribbagemonster_image_4,
+      image: '/assets/projects/cribbagemonster/images/cribbagemonster_4.jpg',
       alt: 'screenshot of the game dashboard',
     },
   ],
